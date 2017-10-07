ba42ed61db2a3d1123a53fbc5dd2f003179b3ab1
Atanas V. Dobrev <atanas.vasilev.dobrev@gmail.com>
2017-10-07 16:31:22 +0300

    look at all the files

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+	http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+	def new
+		@article = Article.new
+	end
+	def index
+	    @articles = Article.all
+	end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def show
+    	@article = Article.find(params[:id])
+  	end
+	
+	def destroy
+	  @article = Article.find(params[:id])
+	  @article.destroy
+	 
+	  redirect_to articles_path
+	end
+	def create  
+		@article = Article.new(params.require(:article).permit(:title, :text))
+	 	if @article.save
+	  		redirect_to @article
+	  	else
+	  		render 'new'
+	  	end
+	end
+	def update
+	    @article = Article.find(params[:id])
+	 
+	    if @article.update(params.require(:article).permit(:title, :text))
+	    	redirect_to @article
+	    else
+	    	render 'edit'
+	  	end
+	end
+end
