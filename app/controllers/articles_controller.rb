81654bd8012b3df6c96d8154392da3db237b15eb
Atanas V. Dobrev <atanas.vasilev.dobrev@gmail.com>
2017-10-07 16:48:22 +0300

    new code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
 	def show
     	@article = Article.find(params[:id])
   	end
-	
+	SEGA RABOTI BRAT
 	def destroy
 	  @article = Article.find(params[:id])
 	  @article.destroy
